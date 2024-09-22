82a0c09b80ecda9b99a586e210ba215387d899e2
afiifatuts <afiifatuts04@gmail.com>
2024-09-23 06:50:34 +0700

    first commit

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,203 @@
+import CustomButton from '@/components/CustomButton';
+import InputField from '@/components/InputField';
+import OAuth from '@/components/OAuth';
+import { icons, images } from '@/constants';
+import { useSignUp } from '@clerk/clerk-expo';
+import { Link, router } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert, Image, Linking, ScrollView, Text, View } from 'react-native';
+import ReactNativeModal from 'react-native-modal';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [showSuccessModal, setShowSuccessModal] = useState(false)
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const [verivication, setVerivication] = useState({
+    state: 'default',
+    error: '',
+    code: '',
+  });
+
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: 'email_code' });
+
+      setVerivication({
+        ...verivication,
+        state: 'pending',
+      });
+    } catch (err: any) {
+      // console.error(JSON.stringify(err, null, 2));
+      Alert.alert("Error",err.errors[0].longMessage)
+    }
+  };
+
+  const onPressVerify = async () => {
+    if (!isLoaded) return;
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verivication.code,
+      });
+
+      if (completeSignUp.status === 'complete') {
+        //TODO : Create Database User
+        await setActive({ session: completeSignUp.createdSessionId });
+        setVerivication({
+          ...verivication,
+          state: 'success',
+        });
+      } else {
+        console.error(JSON.stringify(completeSignUp, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+      setVerivication({
+        ...verivication,
+        state: 'failed',
+        error: err.errors[0].longMessage,
+      });
+    }
+  };
+
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            Already have an account?{' '}
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </View>
+
+        {/* Verification Modal  */}
+
+        <ReactNativeModal
+          isVisible={verivication.state === 'pending'}
+          onModalHide={() =>{
+            if(verivication.state=='success') setShowSuccessModal(true)
+          }
+          }
+        >
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Text className="text-2xl font-JakartaExtraBold mb-2">
+              Verification
+            </Text>
+            <Text className="font-Jakarta mb-5">
+              We've sent a verification code to {form.email}
+            </Text>
+            <InputField
+              label="Code"
+              icon={icons.lock}
+              placeholder="12345"
+              value={verivication.code}
+              keyboardType="numeric"
+              onChangeText={(code) =>
+                setVerivication({
+                  ...verivication,
+                  code,
+                })
+              }
+            />
+
+            {verivication.error && (
+              <Text className="text-red-500 text-sm mt-1">
+                {verivication.error}
+              </Text>
+            )}
+            <CustomButton
+              title="Verify Email"
+              onPress={onPressVerify}
+              className="mt-5 bg-success-500"
+            />
+          </View>
+        </ReactNativeModal>
+
+        <ReactNativeModal isVisible={showSuccessModal}>
+        {/* <ReactNativeModal isVisible={verivication.state==='success'}> */}
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Image
+              source={images.check}
+              className="w-[110px] h-[110px] mx-auto my-5"
+            />
+            <Text className="text-3xl font-JakartaBold text-center">
+              Verified
+            </Text>
+
+            <Text className="text-base text-gray-400 font-Jakarta text-center mt-2">
+              You have successfully verified your account
+            </Text>
+
+            <CustomButton
+              title="Browse Home"
+              onPress={() => {
+                setShowSuccessModal(false)
+                router.push('/(root)/(tabs)/home')}}
+              className="mt-5"
+            />
+          </View>
+        </ReactNativeModal>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default SignUp;
---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Redirect } from 'expo-router';
+import { useAuth } from '@clerk/clerk-expo';
+
+import React from 'react';
+
+const Default = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
+
+  return <Redirect href="/(auth)/welcome" />;
+};
+
+export default Default;
