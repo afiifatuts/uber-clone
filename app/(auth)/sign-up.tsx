c260780d5d4d4b3907faff2ae35e44737bc5a88f
afiifatuts <afiifatuts04@gmail.com>
2024-09-25 07:45:16 +0700

    Do: homepage UI 2:45:57

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,6 +2,7 @@ import CustomButton from '@/components/CustomButton';
 import InputField from '@/components/InputField';
 import OAuth from '@/components/OAuth';
 import { icons, images } from '@/constants';
+import { fetchAPI } from '@/lib/fetch';
 import { useSignUp } from '@clerk/clerk-expo';
 import { Link, router } from 'expo-router';
 import React, { useState } from 'react';
@@ -58,6 +59,16 @@ const SignUp = () => {
 
       if (completeSignUp.status === 'complete') {
         //TODO : Create Database User
+
+        await fetchAPI('/(api)/user',{
+          method:"POST",
+          body:JSON.stringify({
+            name:form.name,
+            email:form.email,
+            clerkId:completeSignUp.createdUserId
+          })
+        })
+
         await setActive({ session: completeSignUp.createdSessionId });
         setVerivication({
           ...verivication,
